f34872cd86393185e1374b6883a7efaba540f0b0
pecata11 <petko.yanakiev.mariadb.com>
2019-05-13 14:48:42 +0300

    change some directories.

--- IOS Projects/Conf411App/ConfComp/UIImageExtras.h
+++
@@ -1,8 +0,0 @@
-// From cscade on iphonedevbook.com forums
-// And Bjorn Sallarp on blog.sallarp.com
-
-@interface UIImage (Extras)
-
-- (UIImage*)imageByScalingAndCroppingForSize:(CGSize)targetSize;
-
-@end
